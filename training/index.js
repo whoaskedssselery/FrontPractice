201e204451e7b07800c114c09cb1250f46f1af9a
whoaskedssselery <ssselery@gmail.com>
2025-11-08 16:23:53 +0300

    js: functions 2/2 +  objects #1/2

--- training/index.js
+++ training/index.js
@@ -1,49 +1,169 @@
-let num1 = Number(prompt("Введите число для проверки: "));
-console.log((Math.abs(num1) == num1) ? "Число положительное" : "Число отрицательное")
+// Задание 1.1
 
-let num2 = Number(prompt("Введите число для проверки: "));
-console.log((num2 % 2 == 0) ? "Число четное" : "Число нечетное");
-
-let a = Number(prompt("Введите первое число для проверки: "));
-let b = Number(prompt("Введите второе число для проверки: "));
-if (a % b == 0)  {
-   console.log(`Число ${a} делится на ${b} без остатка`);
-} else {
-   console.log(`Число ${a} делится на ${b} с остатком: ${a%b}`)
+function log(msg) {
+   const div = document.createElement('div');
+   div.textContent = msg;
+   document.getElementById('log').appendChild(div);
 }
 
-const calculator = (num1, num2, operator) => {
-   switch(operator) {
-      case "+":
-         alert(num1 + num2);
-         break;
-      case "-":
-         alert(num1 - num2);
-         break;
-      case "/":
-         alert(num1 / num2);
-         break;
-      case "%":
-         alert(num1 % num2);
-         break;
-      case "*":
-         alert(num1 * num2);
-         break;
+function loadFile(filename, callback) {
+   const delay = Math.random() * 1000 + 500;
+   setTimeout(() => {
+      if (Math.random() < 0.2) {
+         callback(new Error(`Файл ${filename} не найден`), null);
+      } else {
+         callback(null, { name: filename, content: `Содержимое ${filename}` });
+      }
+   }, delay);
+}
+
+// loadFile("config.json", (err, file) => {
+//    if (err) {
+//       log(`Ошибка: ${err.message}`);
+//    } else {
+//       log(`Успех: ${file.name}`);
+//       log(`Данные: ${file.content}`);
+//    }
+// });
+
+// Задание 1.2
+
+// loadFile("user.json", (err, user) => {
+//   if (err) {
+//     log(err.message);
+//   } else {
+//     log(`Загружен файл: ${user.name}`);
+
+//     loadFile("settings.json", (err, settings) => {
+//       if (err) {
+//         log(err.message);
+//       } else {
+//         log(`Загружен файл: ${settings.name}`);
+
+//         loadFile("data.json", (err, data) => {
+//           if (err) {
+//             log(err.message);
+//           } else {
+//             log(`Загружен файл: ${data.name}`);
+//           }
+//         });
+
+//       }
+//     });
+
+//   }
+// });
+
+// Задание 1.3
+
+// function log(msg) {
+//   const div = document.createElement('div');
+//   div.textContent = msg;
+//   document.getElementById('log').appendChild(div);
+// }
+
+// function loadFile(filename, callback) {
+//   const delay = Math.random() * 1000 + 500;
+//   setTimeout(() => {
+//     if (Math.random() < 0.2) {
+//       callback(new Error(`Ошибка загрузки ${filename}`), null);
+//     } else {
+//       callback(null, { name: filename, content: `Содержимое ${filename}` });
+//     }
+//   }, delay);
+// }
+
+// function step1(error, user) {
+//   if (error) {
+//     log(error.message);
+//   } else {
+//     log(`Загружен файл: ${user.name}`);
+//     loadFile("settings.json", step2);
+//   }
+// }
+
+// function step2(error, settings) {
+//   if (error) {
+//     log(error.message);
+//   } else {
+//     log(`Загружен файл: ${settings.name}`);
+//     loadFile("data.json", step3);
+//   }
+// }
+
+// function step3(error, data) {
+//   if (error) {
+//     log(error.message);
+//   } else {
+//     log(`Загружен файл: ${data.name}`);
+//   }
+// }
+
+// loadFile("user.json", step1);
+
+// Задание 2.1
+
+// let promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     const isSuccess = Math.random() < 0.5;
+//     if (isSuccess) {
+//       resolve("Готово");
+//     } else {
+//       reject(new Error("Ошибка"));
+//     }
+//   }, 1500);
+// });
+
+// promise
+//   .then(result => log(result))
+//   .catch(error => log(error.message))
+//   .finally(() => log("Завершено"));
+
+
+// Задание 2.3
+
+// function delay(ms) {
+//   return new Promise(resolve => setTimeout(resolve, ms));
+// }
+
+// delay(2000).then(() => log("2 сек прошло!"));
+
+// Задание 3.1
+
+function loadFilePromise(filename) {
+ return new Promise((resolve, reject) => {
+ loadFile(filename, (err, data) => {
+   if (err) {
+      reject(err)
+   } else {
+      resolve(data)
    }
+ });
+ });
 }
 
-let calcNum = Number(prompt("Введите первое значение: "));
-let calcOper = prompt("Введите операцию: ");
-let calcNum1 = Number(prompt("Введите второе значение: "));
+loadFilePromise("user.json")
+  .then(file => log(`Загружен: ${file.name}`))
+  .catch(err => log(`${err.message}`))
+  .finally(() => log("Загрузка завершена!"));
 
-calculator(calcNum, calcNum1, calcOper);
+// Задание 2.5
 
-let summa = Number(prompt("Введите общую сумму к оплате: "));
-let percent = Number(prompt("Сколько процентов чаевых вы хотите оставить?: "));
+new Promise(resolve => {
+  setTimeout(() => resolve(1), 1000);
+})
+  .then(result => {
+    log(result);
+    return result * 2;
+  })
+  .then(result => {
+    log(result);
+    return new Promise(resolve => setTimeout(() => resolve(result * 2), 1000));
+  })
+  .then(result => {
+    log(result);
+  });
 
-let result = summa/100*percent;
-console.log(`Чаевые: ${result}` + "\n" + `Общая сумма к оплате с учетом чаевых; ${summa + result}`);
 
-const min = +prompt("min");
-const max = +prompt("max");
-alert(Math.floor(Math.random() * max * +(`1${"0".repeat(max - 1)}`)) + min * +(`1${"0".repeat(min)}`));
+// Задение 3.2
+
