c7dbcb50d99512ba480965892a9e476edc4fc893
whoaskedssselery <ssselery@gmail.com>
2025-11-17 10:37:29 +0300

    js: arrays done!

--- learning/just_js/js/learning/arrays.js
+++ learning/just_js/js/learning/arrays.js
@@ -8,7 +8,10 @@ const matrix = [
 	[6, 7, 8]
 ]
 
-console.log(matrix[2][2])
+console.log(
+	'Какой элемент находится в матрице по адресу [3][3]: ',
+	matrix[2][2]
+)
 
 // Добавление рандомно большого в очереди элемента
 
@@ -42,7 +45,7 @@ const arrayToShow = [
 
 console.log(arrayToShow.join(' '),
 	"Тип после преобразования: ",
-	typeof arrayToShow.join(' ')
+	typeof arrayToShow.join(' ') // string
 	);
 
 // Копирование массивов
@@ -67,4 +70,149 @@ const totalArray = [...testArray]
 
 totalArray.concat(newTestArray, copy)
 
-console.log(totalArray);
+console.log(totalArray)
+
+// Массивы в js #2/2
+
+// Деструктуризация
+
+const someArray = ['Алекс', 28]
+
+const [userName, userAge] = someArray
+
+console.log('name:' , userName)
+console.log('age:', userAge)
+
+// Перебор элементов
+
+const nums = [1, 2, 3, 4, 5]
+
+// forEach принимает функцию
+
+nums.forEach(
+	(number,  index) => {
+		console.log('Элемент: ', number)
+		console.log('Индекс элемента:',  index)
+	}
+)
+
+// Нахождение элемента
+
+const prices = [
+	100, 200, 300, 400, 555,
+	{error: 'NaN'},
+	{error: 'Not added yet'}
+]
+
+console.log('Какая машина стоит 555?', prices.indexOf(555, 2) + 1)
+
+console.log(
+	'Индекс ошибки:',
+	prices.findIndex(
+		(elem) => (typeof elem === 'object' &&  elem.error === 'NaN') // Вернет индекс если 'true'
+	)
+)
+
+// Проверка на наличие элемента в массиве
+
+console.log(
+	'Есть ли у вас машина за 400? ',
+	prices.includes(400)
+)
+
+console.log(
+	'Есть ли ошибки в ценах?',
+	prices.some(
+		(price) => price.error === 'NaN'
+	)
+)
+
+console.log(
+	'Все ли цены указаны в формате числа?',
+	prices.every(
+		(price) => typeof price === 'number'
+	)
+)
+
+console.log(
+	'Лог ошибки:',
+	prices.find(
+		(elem) => (elem.error)
+	)
+)
+
+console.log(
+	'Лог ошибки:',
+	prices.filter(
+		(elem) => (elem.error)
+	)
+)
+
+// Метод Map
+
+const users = [
+	{
+		name: 'Алекс',
+		age: 28,
+		city: 'Moscow'
+	},
+	{
+		name: 'Майкл',
+		age: 30,
+		city: 'Ekaterinburg'
+	},
+	{
+		name: 'Василий',
+		age: 40,
+		city: 'Moscow'
+	},
+	{
+		name: 'Саша',
+		age: 50,
+		city: 'Saint-Petersburg'
+	}
+]
+
+const usersFormatted = users.map(
+	({name, age, city}) => {
+		return `${name}, ${age} лет, живет в г. ${city}`
+	}
+)
+
+console.log(
+	usersFormatted
+)
+
+// Метод reduce
+
+// reduce добавляет дополнительный параметр в начало. Этот параметр принимает начальное значение, которое указано в reduce после ' , ' и изменяет его в зависимости от полученной информации в текущей итерации
+
+console.log(
+	'Средний возраст всех покупателей:',
+	users.reduce(
+		(sum, {age}) => {
+			return sum + age
+		}, 0
+	) / users.length
+)
+
+console.log(
+	'Общий возраст всех покупателей:',
+	users.reduceRight(
+		(sum, {age}) => {
+			console.log(age)
+			return sum + age
+		}, 0
+	)
+)
+
+// Метод reverse()
+
+console.log('Перевернутый массив цен: ', [...prices].reverse())
+console.log('Обычный массив цен без изменений:', prices)
+
+const numbers = [1, 2, 12, 4, -5]
+
+console.log([...numbers].sort(
+	(a, b) => a - b
+))
--- learning/just_js/js/practice/arrays.js
+++ learning/just_js/js/practice/arrays.js
@@ -77,3 +77,76 @@ for (let value of arrayForTest)
 	console.log('Все значения: ', value)
 
 console.log('Кол-во значений в массиве: ', arrayForTest.length)
+
+const nums = [10, 20, 30, 40, 50]
+console.log([...nums].slice(0,3))
+
+const newNums = [1, -5, 3, 0, -2, 8];
+
+console.log(newNums.filter(
+	(number) => number >= 0
+))
+
+const people = [
+	{name: 'Alex', age: 20},
+	{name: 'Mike', age: 30}
+]
+
+console.log(
+	people.map(
+		(user) => user.name
+	)
+)
+
+const ages = [12, 16, 18, 22]
+
+console.log(ages.includes(18))
+
+const users = [
+	{name: 'Alex', city: 'Moscow'},
+	{name: 'Bob', city: 'Kazan'},
+]
+
+console.log(users.find(
+	(user) => user.city === 'Moscow'
+))
+
+const anotherNums = [10, 50, 200, 150]
+
+console.log(anotherNums.findIndex(
+	(number) => number >= 100
+))
+
+const tiredNums = [1, 2, 3, -1]
+
+console.log(
+	tiredNums.every(
+		(number) => number >= 0
+	)
+)
+
+const logs = [
+	{error: false},
+	{error: true},
+	{error: false}
+]
+
+console.log(
+	logs.some(
+		(log) => log.error === true
+	)
+)
+
+const frNums = [10, 20, 30]
+
+console.log(
+	frNums.reduce(
+		(sum, current) => sum + current, 0
+	)
+)
+
+const numsToSort = [5, -1, 10, 2]
+
+console.log([...numsToSort].sort(
+	(a, b) => a - b
+))
