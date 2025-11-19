e515649820357882ec85ef87681a4a807918799e
whoaskedssselery <ssselery@gmail.com>
2025-11-19 19:39:10 +0300

    js: basic class theory + practice done!

--- learning/just_js/js/learning/class.js
+++ learning/just_js/js/learning/class.js
@@ -1,28 +1,37 @@
 // Классы в JS
 
 class User {
-	city = 'Moscow'
+	#city = 'Moscow' // let, const, var не нужно писать
+	// ! Переменную необходимо проинициализировать в классе, чтобы использовать ее в закрытом режиме "#" !
 	
 	constructor(name = undefined, age = undefined) {
-		this.name = name;
-		this.age = age;
+		this.name = name
+		this.age = age
 	}
 	
-	set city(value) {
-		this.city = value;
+	set city(value) { // setter присваивает значения через "="
+		this.#city = value
 	}
 	
-	get city() {
-		return this.city;
+	get city() { // getter работает как обычный метод
+		return this.#city
 	}
 	
-	logInfo() {
-		console.log(`[${this.name}] ${this.age} is logged in from ${this.city}`);
+	logInfo() { // Публичный метод
+		console.log(`[${this.name}] ${this.age}`)
+	}
+	
+	static greeting() { // Публичный метод с использованием "static", что позволяет обращаться к нему без объявления экземпляра
+		console.log('Hello, User!')
+	}
+	
+	#secretLog(city = 'Unknown') { // Метод для использования только внутри класса
+		console.log(`[${this.name}] ${this.age} is logged in from ${this.city}`)
 	}
 }
 
-const ssselery = new User('ssselery', 19);
-const newUser = new User();
+const ssselery = new User('ssselery', 19)
+const newUser = new User()
 
 ssselery.logInfo()
 newUser.logInfo()
@@ -32,3 +41,27 @@ console.log('Город до:', newUser.city)
 newUser.city = 'Rostov'
 
 console.log('Город после:', newUser.city)
+
+// ssselery.secretLog() Вызовет ошибку, т.к метод закрыт для использования вне класса
+
+User.greeting()
+
+// Наследование и переопределение
+
+// Пример наследования
+class Developer extends User{
+		#isDeveloper() {
+			console.log(`${this.name} is developer`)
+		}
+		
+		// Пример переопределения
+		logInfo() {
+			super.logInfo() // Позволяет за одну строчку сохранить функционал прошлой версии метода
+			this.#isDeveloper()
+		}
+}
+
+const newSsselery = new Developer('ssselery', 20)
+
+newSsselery.logInfo()
+
---
+++ learning/just_js/js/practice/class.js
@@ -0,0 +1,76 @@
+// Задание 1
+
+class Product {
+	#discount = 0
+	
+	constructor(name, price) {
+		this.name = name
+		this.price = price
+	}
+	
+	getinfo() {
+		return `Товар: ${this.name}\nЦена: ${this.price}`
+	}
+	
+	set discount(value) {
+		this.#discount = value
+	}
+	
+	get discount() {
+		return this.#discount
+	}
+	
+	getFinalPrice() {
+		return this.price * (1 - this.#discount / 100)
+	}
+	
+	static compare(product1, product2) {
+		if (product1.price < product2.price) {
+			return 'Дешевле'
+		} else if (product1.price > product2.price) {
+			return 'Дороже'
+		} else {
+			return 'Равны'
+		}
+	}
+}
+
+class DigitalProduct extends Product {
+	
+	getFinalPrice() {
+		if (this.discount < 10) {
+			this.discount = 10
+		}
+		return super.getFinalPrice()
+	}
+}
+
+const telega = new Product('telega', 20)
+
+console.log(telega.getinfo())
+
+telega.discount = 10
+console.log(telega.getFinalPrice())
+
+const smartphone = new DigitalProduct('smartphone', 200)
+
+smartphone.discount = 6
+
+console.log(smartphone.getFinalPrice())
+
+console.log(Product.compare(telega, smartphone))
+
+const cart = [
+	new Product("Товар 1", 1500),
+	new Product("Товар 2", 500),
+	new Product("Товар 3", 2000),
+]
+
+let sum = 0
+
+console.log(
+	cart.reduce((sum, item) => sum += item.price, 0)
+)
+
+
+
